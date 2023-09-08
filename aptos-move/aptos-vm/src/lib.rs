a2ace6e9e769be689d8cb5f3b6dcdb70f87baf23
Teng Zhang <rahxephon89@163.com>
2023-09-08 10:49:48 -0400

    comparison testing

--- aptos-move/aptos-vm/src/lib.rs
+++ aptos-move/aptos-vm/src/lib.rs
@@ -119,7 +119,7 @@ pub mod system_module_names;
 pub mod testing;
 pub mod transaction_metadata;
 mod transaction_validation;
-mod verifier;
+pub mod verifier;
 
 pub use crate::aptos_vm::AptosVM;
 use crate::sharded_block_executor::{executor_client::ExecutorClient, ShardedBlockExecutor};
